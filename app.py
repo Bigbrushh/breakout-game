1cbad73d0dfd2b583cae02bd7b31c83c0e53eda9
Bigbrushh <choehwimu@gmail.com>
2024-07-22 10:26:37 +0900

    create canvas

--- app.py
+++ app.py
@@ -2,22 +2,39 @@ import pygame
 pygame.init()
 
 beige_marka = (250, 237, 201)
+ford_dark_charcoal = (50, 50, 50)
 
 screen_width = 800
 screen_height = 600
 
+ball_width = 10
+ball_height = 10
+ball_x = screen_width // 2 - ball_width // 2
+ball_y = screen_width // 2 - ball_height // 2
+ball_dx = 3
+ball_dy = -3
+
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("벽돌 깨기 게임")
+
 running = True
+clock = pygame.time.Clock()
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    ball_x += ball_dx
+    ball_y += ball_dy
+
     screen.fill(beige_marka)
 
+    pygame.draw.ellipse(screen, ford_dark_charcoal, (ball_x, ball_y, ball_width, ball_height))
+
     pygame.display.flip()
+
+    clock.tick(60)
 
 pygame.quit()
